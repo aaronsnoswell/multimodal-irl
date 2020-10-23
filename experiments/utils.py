ce7abd65707b60df88f5a6b9e1d764a954ee2ad2
Aaron Snoswell <aaronsnoswell@gmail.com>
2020-10-23 12:50:48 +1000

    Add median_ci function

--- experiments/utils.py
+++ experiments/utils.py
@@ -27,3 +27,32 @@ def mean_ci(values, confidence_level=0.95):
 
     return mean - ci, mean, mean + ci
 
+
+def median_ci(values, confidence_level=0.95):
+    """Compute median and approximate confidence interval for a list of values
+    
+    The method of computing the CI is taken from
+    https://www.ucl.ac.uk/child-health/short-courses-events/about-statistical-courses/research-methods-and-statistics/chapter-8-content-8
+    
+    Args:
+        values (list): List of float
+        confidence_level (float): Confidence level
+    
+    Returns:
+        (float): Lower approximate confidence interval
+        (float): Median value
+        (float): Upper approximate confidence interval
+    """
+    median = np.median(values)
+    num_repeats = len(values)
+
+    # Compute Z-factor
+    critical_value = 1.0 - confidence_level
+    z_factor = norm().ppf(1 - critical_value / 2)
+
+    # Compute CI rankings
+    low_ci_rank = round(num_repeats / 2 - z_factor * np.sqrt(num_repeats) / 2)
+    high_ci_rank = round(1 + num_repeats / 2 + z_factor * np.sqrt(num_repeats) / 2)
+    values_sorted = sorted(values)
+
+    return values_sorted[low_ci_rank], median, values[high_ci_rank]
