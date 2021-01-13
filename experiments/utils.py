bd50ee7747e956f7762002ff0f6ebba3582f1012
Aaron Snoswell <aaronsnoswell@gmail.com>
2021-01-13 15:48:08 +1000

    Add some commonly used utility functions

--- experiments/utils.py
+++ experiments/utils.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 from scipy.stats import norm
 
@@ -56,3 +57,82 @@ def median_ci(values, confidence_level=0.95):
     values_sorted = sorted(values)
 
     return values_sorted[low_ci_rank], median, values_sorted[high_ci_rank]
+
+
+def get_num_workers(num_jobs, requested_workers=None):
+    """Figure out how many workers to use
+    
+    Args:
+        num_jobs (int): Number of jobs we have to complete
+        requested_workers (int): Number of workers we would like to spin up
+    
+    Returns:
+        (int): Number of workers it is feasible to spin up based on OS scheduling,
+            and/or the size of our workload
+    """
+
+    # Try and determine how many CPUs we are allowed to use
+    num_cpus = (
+        len(os.sched_getaffinity(0))
+        # Ask the (linux) OS how many CPUs wer are scheduled to use
+        if "sched_getaffinity" in dir(os)
+        # If we can't find our scheduled number of CPUs, just use one less than the
+        # system's physical socket count - leave one for GUI, bookkeeping etc.
+        else os.cpu_count() - 1
+    )
+
+    print(f"META: {num_cpus} CPUs available")
+    if requested_workers is not None:
+        num_workers = min(num_cpus, num_jobs, requested_workers)
+    else:
+        num_workers = min(num_cpus, num_jobs)
+
+    return num_workers
+
+
+def replicate_config(cfg, num_times=1):
+    """Replicate a config dictionary some number of times
+    
+    Args:
+        cfg (dict): Base config dictionary
+        num_times (int): Number of repeats
+    
+    Returns:
+        (list): List of duplicated config dictionaries, with an added 'replicate' field.
+    """
+    # Repeat configuration settings across replicates
+    configs = []
+    for replicate in range(num_times):
+        _config = cfg.copy()
+        _config.update({"replicate": replicate})
+        configs.append(_config)
+    return configs
+
+
+def mongo_config(mongodb_config_file="mongodb-config.txt"):
+    """Attempt to load the MongoDB URL from a text file
+    
+    Args:
+        mongodb_config_file (str): Path to a text file containing the MongoDB URL
+    
+    Returns:
+        (str): MongoDB URL, or default URL if path wasn't found
+    """
+    mongodb_url = "localhost:27017"
+    if os.path.exists(mongodb_config_file):
+        with open(mongodb_config_file, "r") as file:
+            mongodb_url = file.readline()
+    return mongodb_url
+
+
+def gt_responsibility_matrix(k, rpm):
+    """Helper to get a ground truth responsibility matrix
+    
+    Args:
+        k (int): Number of clusters
+        rpm (int): Number of rollouts per cluster
+    
+    Returns:
+        (numpy array): Block-diagonal ground truth responsibility matrix
+    """
+    return np.concatenate([np.repeat([np.eye(k)[r, :]], rpm, 0) for r in range(k)], 0,)
